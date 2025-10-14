95345ddd3bcb512816d1c04942b9d8be0f7afa87
ThuyTG <thuytruonggia2@gmail.com>
2025-10-14 11:45:53 +0700

    Add project files.

---
+++ 24DH113182-LTW/Areas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _24DH113182_LTW
+{
+    public class Areas : Controller
+    {
+        // GET: Areas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 24DH113182-LTW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24DH113182_LTW.MvcApplication" Language="C#" %>
